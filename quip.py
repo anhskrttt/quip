f60519fcdf587b3cc0240bf6057a42be1c9b5fab
Anh Duong <duonanh195@gmail.com>
2025-03-28 13:46:36 +0900

    add submodule HIPIFY, update macro

--- quip.py
+++ quip.py
@@ -2,6 +2,8 @@
 import os
 import re
 
+root_path="HIPIFY/docs/reference/tables"
+
 def header(name,swap,infiles,d,pre):
     for infile in infiles:
         with open(infile) as i:
@@ -25,12 +27,12 @@ def header(name,swap,infiles,d,pre):
 # Cuda to Hip
 
 header('cuda2hip/cublas_v2.h',False,
-        ['HIP/docs/markdown/CUBLAS_API_supported_by_HIP.md'],
+        [f'{root_path}/CUBLAS_API_supported_by_HIP.md'],
         dict(),
         ['#include <hipblas.h>'])
 
 header('cuda2hip/cuda.h',False,
-        ['HIP/docs/markdown/CUDA_Driver_API_functions_supported_by_HIP.md','HIP/docs/markdown/CUDA_Runtime_API_functions_supported_by_HIP.md'],
+        [f'{root_path}/CUDA_Driver_API_functions_supported_by_HIP.md',f'{root_path}/CUDA_Runtime_API_functions_supported_by_HIP.md'],
         {'__shfl_sync':'__shfl','__shfl_up_sync':'__shfl_up','__shfl_down_sync':'__shfl_down','__shfl_xor_sync':'__shfl_xor'},
         ['#include <hip/hip_runtime.h>'])
 
@@ -43,51 +45,51 @@ header('cuda2hip/cuda_runtime_api.h',False,
         ['#include <cuda.h>'])
 
 header('cuda2hip/cufft.h',False,
-        ['HIP/docs/markdown/CUFFT_API_supported_by_HIP.md'],
+        [f'{root_path}/CUFFT_API_supported_by_HIP.md'],
         dict(),
         ['#include <hipfft.h>'])
 
 header('cuda2hip/curand.h',False,
-        ['HIP/docs/markdown/CURAND_API_supported_by_HIP.md'],
+        [f'{root_path}/CURAND_API_supported_by_HIP.md'],
         dict(),
         ['#include <hiprand.hpp>'])
 
 header('cuda2hip/cusparse.h',False,
-        ['HIP/docs/markdown/CUSPARSE_API_supported_by_HIP.md'],
+        [f'{root_path}/CUSPARSE_API_supported_by_HIP.md'],
         dict(),
         ['#include <hipsparse.h>'])
 
 # Hip to Cuda
 
 header('hip2cuda/hipblas.h',True,
-        ['HIP/docs/markdown/CUBLAS_API_supported_by_HIP.md'],
+        [f'{root_path}/CUBLAS_API_supported_by_HIP.md'],
         dict(),
         ['#include <cublas_v2.h>'])
 
 header('hip2cuda/hip/hip_runtime.h',True,
-        ['HIP/docs/markdown/CUDA_Runtime_API_functions_supported_by_HIP.md',
-            'HIP/docs/markdown/CUDA_Driver_API_functions_supported_by_HIP.md'],
+        [f'{root_path}/CUDA_Runtime_API_functions_supported_by_HIP.md',
+            f'{root_path}/CUDA_Driver_API_functions_supported_by_HIP.md'],
         {'__shfl':'__shfl_sync','__shfl_up':'__shfl_up_sync','__shfl_down':'__shfl_down_sync','__shfl_xor':'__shfl_xor_sync',
             'hipLaunchKernelGGL(F,G,B,M,S,...)':'F<<<G,B,M,S>>>(__VA_ARGS__)'},
         ['#include <cuda.h>','#include <cuda_runtime.h>'])
 
 header('hip2cuda/hip/hip_runtime_api.h',True,
-        ['HIP/docs/markdown/CUDA_Runtime_API_functions_supported_by_HIP.md'],
+        [f'{root_path}/CUDA_Runtime_API_functions_supported_by_HIP.md'],
         dict(),
         ['#include <cuda_rutime_api.h>'])
 
 header('hip2cuda/hipfft.h',True,
-        ['HIP/docs/markdown/CUFFT_API_supported_by_HIP.md'],
+        [f'{root_path}/CUFFT_API_supported_by_HIP.md'],
         dict(),
         ['#include <cufft.h>'])
 
 header('hip2cuda/hiprand.h',True,
-        ['HIP/docs/markdown/CURAND_API_supported_by_HIP.md'],
+        [f'{root_path}/CURAND_API_supported_by_HIP.md'],
         dict(),
         ['#include <curand.hpp>'])
 
 header('hip2cuda/hipsparse.h',True,
-        ['HIP/docs/markdown/CUSPARSE_API_supported_by_HIP.md'],
+        [f'{root_path}/CUSPARSE_API_supported_by_HIP.md'],
         dict(),
         ['#include <cusparse.h>'])
 
